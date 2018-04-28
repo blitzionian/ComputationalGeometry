76548359380db4ece922198a943f5db5168e1fdc
Markus Hüttner <markus.huettner@mailbox.org>
2018-04-28 16:02:50 +0200

    Added Project for P2

---
+++ P2/src/Praktikum2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+const string FILE_1 = "input/s_1000_1.dat";
+
+int main() {
+	cout << "Hallo!" << endl;
+	return 0;
+}
