71d80c40504f6a7caf471bb3802fdc9eb46e756e
Markus Hüttner <markus.huettner@mailbox.org>
2018-05-03 16:44:44 +0200

    P2 added stroing to map, not ready yet.

--- P2/src/Praktikum2.cpp
+++ P2/src/Praktikum2.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <vector>
 #include <chrono>
+#include <iterator>
+#include <map>
 
 #include "point.hpp"
 #include "line.hpp"
@@ -23,29 +25,46 @@ int main() {
 	image = nsvgParseFromFile(FILE_1, "px", 96);
 	printf("size: %f x %f\n", image->width, image->height);
 
+	map<char*, vector<Line>> polygonMap;
+
 	NSVGshape* shape;
 	for (shape = image->shapes; shape != NULL; shape = shape->next) {
 		NSVGpath* path;
-		cout << shape->id << endl;
+
+		vector<Line> newBundesland;
+		polygonMap.insert(pair<char*, vector<Line>>(shape->id, newBundesland));
 
 		int count = 0;
 		for (path = shape->paths; path != NULL; path = path->next) {
 			count++;
 
+			Point* firstPoint;
 			Point* lastPoint;
 
 			for (int i = 0; i < path->npts - 1; i += 6) {
-
 				Point nextPoint(path->pts[i], path->pts[i + 1]);
 
-				if (strcmp(shape->id, "Berlin") == 0) {
-					cout << nextPoint.toString() << endl;
+				if (firstPoint == NULL) {
+					firstPoint = &nextPoint;
+				}
+
+				if (lastPoint != NULL) {
+					Line newLine(*lastPoint, nextPoint);
+					newBundesland.push_back(newLine);
 				}
 
 				lastPoint = &nextPoint;
 			}
+
+			newBundesland.push_back(Line(*lastPoint, *firstPoint));
 		}
 	}
+
+	map<char*, vector<Line>>::iterator itr;
+	for (itr = polygonMap.begin(); itr != polygonMap.end(); ++itr) {
+		cout << itr->first << '\n';
+	}
+	cout << endl;
 
 //	nsvgDelete(image);
 }
