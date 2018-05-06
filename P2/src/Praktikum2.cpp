6bd34d0d96509e18a84083fa462a15acb0806f23
Markus Hüttner <markus.huettner@mailbox.org>
2018-05-06 18:12:18 +0200

    Added reading of Polygon from File.

--- P2/src/Praktikum2.cpp
+++ P2/src/Praktikum2.cpp
@@ -20,19 +20,36 @@ using namespace std;
 
 const char* FILE_1 = "input/DeutschlandMitStaedten.svg";
 
+map<string*, vector<Line*>*>* readPolygonFromSvg(const char* svgFile);
+
 int main() {
+	map<string*, vector<Line*>*>* polygonMap = readPolygonFromSvg(FILE_1);
+
+	map<string*, vector<Line*>*>::iterator itr;
+	for (itr = polygonMap->begin(); itr != polygonMap->end(); ++itr) {
+		cout << *itr->first << '\n';
+	}
+
+	cout << endl;
+}
+
+/**
+ * Liest alle Pfade aus der gegebenen SVG Datei. Die Pfade sind in der zurückgegebenen Map enthalten.
+ * Der Key der Map ist die id des Pfades. (In unserem Fall der Name des Bundeslandes)
+ */
+map<string*, vector<Line*>*>* readPolygonFromSvg(const char* svgFile) {
 	struct NSVGimage* image;
 	image = nsvgParseFromFile(FILE_1, "px", 96);
-	printf("size: %f x %f\n", image->width, image->height);
 
-	map<char*, vector<Line>> polygonMap;
+	map<string*, vector<Line*>*>* polygonMap = new map<string*, vector<Line*>*>();
 
 	NSVGshape* shape;
 	for (shape = image->shapes; shape != NULL; shape = shape->next) {
 		NSVGpath* path;
 
-		vector<Line> newBundesland;
-		polygonMap.insert(pair<char*, vector<Line>>(shape->id, newBundesland));
+		vector<Line*>* newBundesland = new vector<Line*>();
+		string* name = new string(shape->id, 63);
+		polygonMap->insert(pair<string*, vector<Line*>*>(name, newBundesland));
 
 		int count = 0;
 		for (path = shape->paths; path != NULL; path = path->next) {
@@ -49,23 +66,17 @@ int main() {
 				}
 
 				if (lastPoint != NULL) {
-					Line newLine(*lastPoint, nextPoint);
-					newBundesland.push_back(newLine);
+					Line* newLine = new Line(*lastPoint, nextPoint);
+					newBundesland->push_back(newLine);
 				}
 
 				lastPoint = &nextPoint;
 			}
 
-			newBundesland.push_back(Line(*lastPoint, *firstPoint));
+			newBundesland->push_back(new Line(*lastPoint, *firstPoint));
 		}
 	}
 
-	map<char*, vector<Line>>::iterator itr;
-	for (itr = polygonMap.begin(); itr != polygonMap.end(); ++itr) {
-		cout << itr->first << '\n';
-	}
-	cout << endl;
-
-//	nsvgDelete(image);
+	return polygonMap;
 }
 
