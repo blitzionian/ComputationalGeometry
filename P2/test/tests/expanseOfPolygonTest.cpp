f3ecc4a230806b2e1519ce7c409ab63174d566c4
MarkusH <markus.huettner@mailbox.org>
2018-05-13 20:25:48 +0200

    Added tests.

--- P2/test/tests/expanseOfPolygonTest.cpp
+++ P2/test/tests/expanseOfPolygonTest.cpp
@@ -40,6 +40,26 @@ namespace {
 		ASSERT_EQ(0.75, polygon.getExpanse());
 	}
 
+	TEST(Polygon_Expanse_Test, Square_With_2_Triangles) {
+		Line l1(*new Point(0, 1), *new Point(0.5, 0.5));
+		Line l2(l1.getEndPoint(), *new Point(0, 0));
+		Line l3(l2.getEndPoint(), *new Point(1, 0));
+		Line l4(l3.getEndPoint(), *new Point(0.5, 0.5));
+		Line l5(l4.getEndPoint(), *new Point(1, 1));
+		Line l6(l5.getEndPoint(), l1.getStartPoint());
+
+		vector<Line*> polygonEdges;
+		polygonEdges.push_back(&l1);
+		polygonEdges.push_back(&l2);
+		polygonEdges.push_back(&l3);
+		polygonEdges.push_back(&l4);
+		polygonEdges.push_back(&l5);
+		polygonEdges.push_back(&l6);
+
+		Polygon polygon(&polygonEdges);
+		ASSERT_EQ(0.5, polygon.getExpanse());
+	}
+
 	TEST(Polygon_Expanse_Test, Triangle) {
 		Line l1(*new Point(0, 1), *new Point(0, 0));
 		Line l2(l1.getEndPoint(), *new Point(1, 0));
--- P2/test/tests/lineTest.cpp
+++ P2/test/tests/lineTest.cpp
@@ -2,17 +2,73 @@
 #include <line.hpp>
 
 namespace {
+	TEST(Line_Test, Expanse1) {
+		Point startPoint(0, 0);
+		Point endPoint(1, 0);
+		Line line(startPoint, endPoint);
+
+		ASSERT_DOUBLE_EQ(0, line.getExpanseOverXLine());
+	}
+
+	TEST(Line_Test, Expanse2) {
+		Point startPoint(0, 1);
+		Point endPoint(1, 1);
+		Line line(startPoint, endPoint);
+
+		ASSERT_DOUBLE_EQ(-1, line.getExpanseOverXLine());
+	}
+
+	TEST(Line_Test, Expanse3) {
+		Point startPoint(1, 1);
+		Point endPoint(0, 1);
+		Line line(startPoint, endPoint);
+
+		ASSERT_DOUBLE_EQ(1, line.getExpanseOverXLine());
+	}
+
+	TEST(Line_Test, Expanse4) {
+		Point startPoint(0, 1);
+		Point endPoint(0.5, 0.5);
+		Line line(startPoint, endPoint);
+
+		ASSERT_DOUBLE_EQ(-0.375, line.getExpanseOverXLine());
+	}
+
+	TEST(Line_Test, Expanse5) {
+		Point startPoint(0.5, 0.5);
+		Point endPoint(0, 0);
+		Line line(startPoint, endPoint);
+
+		ASSERT_DOUBLE_EQ(0.125, line.getExpanseOverXLine());
+	}
+
+	TEST(Line_Test, Expanse6) {
+		Point startPoint(0.5, 0.5);
+		Point endPoint(1, 1);
+		Line line(startPoint, endPoint);
+
+		ASSERT_DOUBLE_EQ(-0.375, line.getExpanseOverXLine());
+	}
+
+	TEST(Line_Test, Expanse7) {
+		Point startPoint(1, 1);
+		Point endPoint(0, 1);
+		Line line(startPoint, endPoint);
+
+		ASSERT_DOUBLE_EQ(1, line.getExpanseOverXLine());
+	}
+
 	TEST(Line_Test, Getter) {
 		Point expectedStartPoint(1, 2);
 		Point expectedEndPoint(3, 4);
 		Line line(expectedStartPoint, expectedEndPoint);
 
 		Point actualStartPoint = line.getStartPoint();
-		ASSERT_EQ(expectedStartPoint.getX(), actualStartPoint.getX());
-		ASSERT_EQ(expectedStartPoint.getY(), actualStartPoint.getY());
+		ASSERT_DOUBLE_EQ(expectedStartPoint.getX(), actualStartPoint.getX());
+		ASSERT_DOUBLE_EQ(expectedStartPoint.getY(), actualStartPoint.getY());
 
 		Point actualEndPoint = line.getEndPoint();
-		ASSERT_EQ(expectedEndPoint.getX(), actualEndPoint.getX());
-		ASSERT_EQ(expectedEndPoint.getY(), actualEndPoint.getY());
+		ASSERT_DOUBLE_EQ(expectedEndPoint.getX(), actualEndPoint.getX());
+		ASSERT_DOUBLE_EQ(expectedEndPoint.getY(), actualEndPoint.getY());
 	}
 }
