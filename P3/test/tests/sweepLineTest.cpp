f9b8308197efb3ebdf93f39d55cbf55f50b48887
MarkusH <markus.huettner@mailbox.org>
2018-06-02 14:35:00 +0200

    Fixed error in test data.

--- P3/test/tests/sweepLineTest.cpp
+++ P3/test/tests/sweepLineTest.cpp
@@ -32,7 +32,7 @@ namespace {
 		// Triangle 2
 		Line line4 = Line(new Point(1.2, 11), new Point(4.1, 14));
 		Line line5 = Line(new Point(3.2, 14), new Point(6.2, 11));
-		Line line6 = Line(new Point(3.3, 12), new Point(6.3, 12.1));
+		Line line6 = Line(new Point(1.3, 12), new Point(6.3, 12.1));
 
 		vector<Line*> lines;
 		lines.push_back(&line1);
@@ -57,5 +57,26 @@ namespace {
 		ASSERT_TRUE(cps->contains(Crosspoint(&line6, &line4)));
 
 		ASSERT_FALSE(cps->contains(Crosspoint(&line1, &line4)));
+	}
+
+	TEST(SweepLine, TestTriangle2) {
+		// Triangle 2
+		Line line4 = Line(new Point(1.2, 11), new Point(4.1, 14));
+		Line line5 = Line(new Point(3.2, 14), new Point(6.2, 11));
+		Line line6 = Line(new Point(1.3, 12), new Point(6.3, 12.1));
+
+		vector<Line*> lines;
+		lines.push_back(&line4);
+		lines.push_back(&line5);
+		lines.push_back(&line6);
+
+		SweepLine sl(lines);
+		Crosspoints* cps = sl.calculateResult();
+
+		ASSERT_EQ(sl.getCrossCount(), 3);
+
+		ASSERT_TRUE(cps->contains(Crosspoint(&line4, &line5)));
+		ASSERT_TRUE(cps->contains(Crosspoint(&line5, &line6)));
+		ASSERT_TRUE(cps->contains(Crosspoint(&line6, &line4)));
 	}
 }
