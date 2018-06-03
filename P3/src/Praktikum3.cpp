17d8102f5fa621496c86976c926a079fbd6462e2
MarkusH <markus.huettner@mailbox.org>
2018-06-03 16:07:36 +0200

    Updated gitignore, Change order of files.

--- P3/src/Praktikum3.cpp
+++ P3/src/Praktikum3.cpp
@@ -10,9 +10,9 @@ using namespace std;
 using namespace std::chrono;
 
 const string FILE_1 = "input/s_1000_1.dat";
-const string FILE_2 = "input/s_10000_1.dat";
-const string FILE_3 = "input/s_100000_1.dat";
-const string FILE_4 = "input/s_1000_10.dat";
+const string FILE_2 = "input/s_1000_10.dat";
+const string FILE_3 = "input/s_10000_1.dat";
+const string FILE_4 = "input/s_100000_1.dat";
 
 int main() {
 	calculateFile(FILE_1);
