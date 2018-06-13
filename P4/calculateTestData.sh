07c0643fb095d6c5b9dc7f7543966056b5344bfa
MarkusH <markus.huettner@mailbox.org>
2018-06-13 23:06:41 +0200

    added qhull binaries, added shell files, updated gitignore

---
+++ P4/calculateTestData.sh
@@ -0,0 +1,35 @@
+./qhull/qconvex < ./testdata/d2_1k.dat >> ./results/results_2d.txt
+./qhull/qhull/qconvex < ./testdata/d2_10k.dat >> ./results/results_2d.txt
+./qhull/qconvex < ./testdata/d2_100k.dat >> ./results/results_2d.txt
+./qhull/qconvex < ./testdata/d2_1000k.dat >> ./results/results_2d.txt
+
+./qhull/qconvex < ./testdata/d3_1k.dat >> ./results/results_3d.txt
+./qhull/qconvex < ./testdata/d3_1k.dat >> ./results/results_3d.txt
+./qhull/qconvex < ./testdata/d3_10k.dat >> ./results/results_3d.txt
+./qhull/qconvex < ./testdata/d3_100k.dat >> ./results/results_3d.txt
+./qhull/qconvex < ./testdata/d3_1000k.dat >> ./results/results_3d.txt
+
+./qhull/qconvex < ./testdata/d4_1k.dat >> ./results/results_4d.txt
+./qhull/qconvex < ./testdata/d4_10k.dat >> ./results/results_4d.txt
+./qhull/qconvex < ./testdata/d4_100k.dat >> ./results/results_4d.txt
+./qhull/qconvex < ./testdata/d4_1000k.dat >> ./results/results_4d.txt
+
+./qhull/qconvex < ./testdata/d5_1k.dat >> ./results/results_5d.txt
+./qhull/qconvex < ./testdata/d5_10k.dat >> ./results/results_5d.txt
+./qhull/qconvex < ./testdata/d5_100k.dat >> ./results/results_5d.txt
+./qhull/qconvex < ./testdata/d5_1000k.dat >> ./results/results_5d.txt
+
+./qhull/qconvex < ./testdata/d6_1k.dat >> ./results/results_6d.txt
+./qhull/qconvex < ./testdata/d6_10k.dat >> ./results/results_6d.txt
+./qhull/qconvex < ./testdata/d6_100k.dat >> ./results/results_6d.txt
+./qhull/qconvex < ./testdata/d6_1000k.dat >> ./results/results_6d.txt
+
+./qhull/qconvex < ./testdata/d7_1k.dat >> ./results/results_7d.txt
+./qhull/qconvex < ./testdata/d7_10k.dat >> ./results/results_7d.txt
+./qhull/qconvex < ./testdata/d7_100k.dat >> ./results/results_7d.txt
+./qhull/qconvex < ./testdata/d7_1000k.dat >> ./results/results_7d.txt
+
+./qhull/qconvex < ./testdata/d8_1k.dat >> ./results/results_8d.txt
+./qhull/qconvex < ./testdata/d8_10k.dat >> ./results/results_8d.txt
+./qhull/qconvex < ./testdata/d8_100k.dat >> ./results/results_8d.txt
+./qhull/qconvex < ./testdata/d8_1000k.dat >> ./results/results_8d.txt
---
+++ P4/createTestData.sh
@@ -0,0 +1,34 @@
+./qhull/rbox  1000 D2 B10 > ./testdata/d2_1k.dat
+./qhull/rbox  10000 D2 B10 > ./testdata/d2_10k.dat
+./qhull/rbox  100000 D2 B10 > ./testdata/d2_100k.dat
+./qhull/rbox  1000000 D2 B10 > ./testdata/d2_1000k.dat
+
+./qhull/rbox  1000000 D3 B10 > ./testdata/d3_1000k.dat
+./qhull/rbox  100000 D3 B10 > ./testdata/d3_100k.dat
+./qhull/rbox  10000 D3 B10 > ./testdata/d3_10k.dat
+./qhull/rbox  1000 D3 B10 > ./testdata/d3_1k.dat
+
+./qhull/rbox  1000 D4 B10 > ./testdata/d4_1k.dat
+./qhull/rbox  10000 D4 B10 > ./testdata/d4_10k.dat
+./qhull/rbox  100000 D4 B10 > ./testdata/d4_100k.dat
+./qhull/rbox  1000000 D4 B10 > ./testdata/d4_1000k.dat
+
+./qhull/rbox  1000000 D5 B10 > ./testdata/d5_1000k.dat
+./qhull/rbox  100000 D5 B10 > ./testdata/d5_100k.dat
+./qhull/rbox  10000 D5 B10 > ./testdata/d5_10k.dat
+./qhull/rbox  1000 D5 B10 > ./testdata/d5_1k.dat
+
+./qhull/rbox  1000 D6 B10 > ./testdata/d6_1k.dat
+./qhull/rbox  10000 D6 B10 > ./testdata/d6_10k.dat
+./qhull/rbox  100000 D6 B10 > ./testdata/d6_100k.dat
+./qhull/rbox  1000000 D6 B10 > ./testdata/d6_1000k.dat
+
+./qhull/rbox  1000 D7 B10 > ./testdata/d7_1k.dat
+./qhull/rbox  10000 D7 B10 > ./testdata/d7_10k.dat
+./qhull/rbox  100000 D7 B10 > ./testdata/d7_100k.dat
+./qhull/rbox  1000000 D7 B10 > ./testdata/d7_1000k.dat
+
+./qhull/rbox  1000000 D8 B10 > ./testdata/d8_1000k.dat
+./qhull/rbox  100000 D8 B10 > ./testdata/d8_100k.dat
+./qhull/rbox  10000 D8 B10 > ./testdata/d8_10k.dat
+./qhull/rbox  1000 D8 B10 > ./testdata/d8_1k.dat
